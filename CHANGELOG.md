2cd4bfb87dd95ab7a49bb9d5a55a31b8c05c17bd
Jake Trapp <jacob.trapp@colepublishing.com>
2026-03-19 14:08:35 -0500

    Initial Commit

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release Notes for Engagement
+
+## 1.0.0
+- Initial release
---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace jtdev\craftengagement\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * Engagement settings
+ */
+class Settings extends Model
+{
+}
